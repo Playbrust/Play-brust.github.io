aab3e4d8849eb47135b5fd55a5ce4d767eff8746
Playbrust <samitamalik192@gmail.com>
2025-05-19 11:21:30 +0530

    Play brust
    Play Burst is a free, customizable gaming platform that lets you host and play HTML5 games in one place. Inspired by popular gaming sites, but 100% open-source and ad-free.

---
+++ index (2).js
@@ -0,0 +1,9 @@
+const games = [
+  {
+    id: 'space-invaders',
+    name: 'Space Invaders',
+    path: '/games/space-invaders',
+    thumbnail: '/assets/game-thumbnails/space-invaders.jpg'
+  }
+  // Add new games here
+];
